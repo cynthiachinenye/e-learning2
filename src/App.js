b8d13329df33a70adafbd412f9b4ea3a16124557
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-26 19:38:12 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from  'react-bootstrap/Container'
+import NavbarBrand from 'react-bootstrap/esm/NavbarBrand'
+
 
 const Navbar = () => {
   return (
     <div>
+    <Navbar  className='nav bg-light'>
+     <Container>
+     <NavbarBrand href='#home'> Navbar</NavbarBrand>
+     <Nav>
+     
+     </Nav>
+     
+     </Container>
+    
+    </Navbar>
       
     </div>
   )
