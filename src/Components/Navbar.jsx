b56f302537f406f17a26b5837ed51b819353f119
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-06 19:55:33 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 
 
@@ -7,27 +8,31 @@ const Navbar = () => {
 
   return (
     <div>
-    <nav class="navbar navbar-expand-lg navbar-light justify-content-space-between">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src='../Assets/logo.png' alt="logo"/></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light ">
+    <div className="container-fluid  d-flex justify-content-between">
+     <div >
+     <a className="navbar-brand ms-3 me-5" href="#"><img src='../Assets/logo.png' alt="logo"/></a>
+     </div>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <div className="collapse navbar-collapse" id="navbarText  text-center">
+        <ul className="navbar-nav ms-2 mb-2 mb-lg-0  ">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Profile</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Courses</a>
           </li>
+          <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
         </ul>
-        <span class="navbar-text">
-          Navbar text with an inline element
-        </span>
+        <button>Sign In</button>
+       
       </div>
     </div>
   </nav>  
