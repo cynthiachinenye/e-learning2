665355612cb5dc42a385c4acfbe17e75872f401f
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-08 08:53:57 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import '../Styles/navbar.css'
+import {Link} from 'react'
 
 
 
@@ -9,17 +10,17 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light ">
-        <div className=" container-fluid  d-flex justify-content-between p-3">
+      <nav className="navbar navbar-expand-lg navbar-light  d-flex justify-content-between p-3">
+        <div className=" container-fluid  ">
    
           <div >
             <a className="navbar-brand ms-3 me-5" href="#"><img src='../Assets/logo.png' alt="logo" /></a>
           </div>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-pressed= " true" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"  aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse ps-5 text-dark md-5" id="navbarText ">
-            <ul className="navbar-nav ms-2 mb-2 mb-lg-0 fs-6 ">
+          <div className="collapse navbar-collapse " id="navbarTogglerDemo01 ">
+            <ul className="navbar-nav ms-2 mb-2 mb-lg-0 fs-6  ps-5 text-dark md-5">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
