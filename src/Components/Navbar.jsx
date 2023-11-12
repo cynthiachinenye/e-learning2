ea87cb2e2cdc312948b11165dbf20b8d1851004c
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-12 19:05:19 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
 
-const Navbar = () => {
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../Styles/navbar.css'
+
+
+const Menu = () => {
+  
   return (
     <div>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      
+    <Navbar expand="lg" className="navbar ">
+    <Container>
+      <img src='./Assets/Logo.png' alt='Logo' className="logo"/>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-5">
+          <Nav.Link href="#home  active">Home</Nav.Link>
+          <Nav.Link href="#profile">Profile</Nav.Link>
+          <Nav.Link href="#course">Courses</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <button type='submit' className='btn btn-light text-primary bg-light'>Sign Up</button>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+    
     </div>
   )
 }
 
-export default Navbar
+export default Menu
+
