bf1eaea975992fa93650ed39517531031f5cb9b5
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-13 17:27:12 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Navbar from "./Components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Router, Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Router>
+      <
+      
+      
+      </Router>
       
     </div>
   );
