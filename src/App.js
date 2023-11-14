a475abcf11241a8dc113ccde13826672807dff69
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-14 09:47:01 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Navbar from "./Components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Router, Route,Routes} from 'react-router-dom';
+import Header from "./Pages/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Router>
-      <
+      <Route>
+      <Routes element={<Header/>}/>
+      
+      
+      </Route>
       
       
       </Router>
