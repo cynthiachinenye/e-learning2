271a62d3a59bd843bca846d661211b4110f6e57f
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-21 01:08:05 +0100

    homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
   
     <div className='container-sm  d-flex justify-content-around '>
-    <div className='row  alin-item-center '  >
+    <div className='d-flex '  >
     <div className='left-side my-3 text-left col-md-5 text-md-start'>
     <h1 className=' fs-2 '>Discover best classes for the best learning</h1>
     <p className='pb-3'>We designed Connections Academy to give students all across the nation a tuition-free online public school that lets them learn.</p>
@@ -14,7 +14,7 @@ const Home = () => {
     
     </div>
     <div className='right-side'>
-    <img src='./Assets/Image and card.png' alt='image' className=" img-fluid w-25"/>
+    <img src='./Assets/Image and card.png' alt='image' className=" img-fluid w-75 ms-5"/>
     
     </div>
     
