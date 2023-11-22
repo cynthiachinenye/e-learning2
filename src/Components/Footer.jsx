f2dbac08ca8a5de3df3b0a80fbbf761e09e53510
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-22 02:23:12 +0100

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import '../Styles/footer.css'
 
 const Footer = () => {
   return (
+    <div className='container'>
+    <div className='d-flex'>
+    <div className=''>
+    <img src='./Assets/Logo.png' alt='logo'/>
+    <p >Our motto to fulfill customer demand by making them satisfied with growing their business.</p>
+    </div>
+    
     <div>
+    <h5>About</h5>
+   <a href='#' className='link' >About Us</a>
+    <a href='#' className='link'>Features</a>
+     <a href='#' className='link'>News</a>
+      <a href='#' className='link'>Careers</a>
+       <a href='#' className='link'>FAQ</a>
+    </div>
+
+    <div>
+    <h5>Support</h5>
+   <a href='#' className='link' >Account</a>
+    <a href='#' className='link'>Support Center</a>
+     <a href='#' className='link'>Feedback</a>
+      <a href='#' className='link'>Contact Us</a>
+       <a href='#' className='link'>Accesbility</a>
+    </div>
+    
+    </div>
       
     </div>
   )
