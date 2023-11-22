39e35ef19c6e4a501a6afd432728572290479cbd
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-22 10:49:21 +0100

    footer

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Courses = () => {
   return (
+    <div className='container'>
     <div>
+    <h1>HOW OUR ONLINE PUBLIC SCHOOL WORKS</h1>
+    <button className='btn'>View all courses</button>
+    </div>
       
     </div>
   )
