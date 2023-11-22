0aa200468595e3e27d41869ede315b63f9e85d7a
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-22 12:33:45 +0100

    course

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -3,11 +3,22 @@ import React from 'react'
 const Courses = () => {
   return (
     <div className='container m-5 pt-5 '>
-    <div className='d-flex align-items-center justify-content-between'>
-    <h1 className='w-25 fs-5 fw-bold'>HOW OUR ONLINE PUBLIC SCHOOL WORKS</h1>
-    <button className='btn btn-sm  btn-outline-primary' type="button">View all courses</button>
-    </div>
+      <div className='d-flex align-items-center justify-content-around'>
+        <h1 className='w-25 fs-5 fw-bold'>HOW OUR ONLINE PUBLIC SCHOOL WORKS</h1>
+        <button className='btn btn-sm  btn-outline-primary' type="button">View all courses</button>
+
+      </div>
+      <div className='container-fluid'>
+      <div>
+        <img src='./Assets/Image.png' alt='slide'/>
+
+        <h5>Hands-On Phython & R In Data Science</h5>
+        <div></div>
+        <button>Add to Cart</button>
+      </div>
       
+      </div>
+
     </div>
   )
 }
